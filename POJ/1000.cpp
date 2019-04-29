03b853ec02163f5ffe718d7c2fa92740bce8f1e7
sudoku.huang <sudoku.huang@gmail.com>
2019-04-29 12:42:20 +0800

    [POJ] 1000.

---
+++ POJ/1000.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+    > File Name: 1000.cpp
+    > Author: sudoku.huang
+    > Mail: sudoku.huang@gmail.com 
+    > Created Time: Mon Apr 29 12:12:40 2019
+ ************************************************************************/
+
+#include <stdio.h>
+
+int main(int argc, char * argv[])
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d\n", a + b);
+    return 0;
+}
+
